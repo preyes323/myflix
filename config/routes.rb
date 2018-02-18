4e50e7a851330f0f2ab94ad0adad250accdc593d
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2018-02-18 13:42:48 +0800

    Completes category and videos view/association

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'ui(/:action)', controller: 'ui'
-
   get '/home' => 'videos#index', as: 'home'
+
   resources :videos, only: %i[index show]
+  resources :categories, only: :show
 end
