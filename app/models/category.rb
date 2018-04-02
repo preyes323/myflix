cb86f74b22b676557329ee5d677dfb3b3b2ab399
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2018-04-02 13:07:34 +0800

    Completes recent_videos functionality

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,6 @@ class Category < ApplicationRecord
   has_many :videos
 
   def recent_videos
+    Video.where(category: self).order(created_at: :desc, title: :asc).limit(6)
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Category do
       v8 = Video.create title: 'monk', description: 'good movie', category: @category
 
       results = @category.recent_videos
-      expect(results).to eq([v8, v7, v6, v5, v4, v3, v2])
+      expect(results).to eq([v8, v7, v6, v5, v4, v3])
     end
     it 'returns all videos if less than or equal 6 videos exist in descending created at' do
       v1 = Video.create title: 'monk', description: 'good movie', category: @category
@@ -29,11 +29,13 @@ RSpec.describe Category do
       results = @category.recent_videos
       expect(results).to eq([v3, v2, v1])
     end
+    
     context 'with the same created at' do
       it 'sorts by created at and then sorts alphabetically by title' do
-        v1 = Video.create title: 'mank', description: 'good movie', category: @category, created_at: 1.day.ago
-        v2 = Video.create title: 'mink', description: 'good movie', category: @category, created_at: 1.day.ago
-        v3 = Video.create title: 'monk', description: 'good movie', category: @category, created_at: 1.day.ago
+        created_at = 1.day.ago
+        v1 = Video.create title: 'mank', description: 'good movie', category: @category, created_at: created_at
+        v2 = Video.create title: 'mink', description: 'good movie', category: @category, created_at: created_at
+        v3 = Video.create title: 'monk', description: 'good movie', category: @category, created_at: created_at
 
         results = @category.recent_videos
         expect(results).to eq([v1, v2, v3])
