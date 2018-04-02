4f8e770ea057eac6b70954788ea51581311e97b9
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2018-04-02 12:14:00 +0800

    Adds spec for recent_videoes; WIP: implement recent videos

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,10 +4,40 @@ RSpec.describe Category do
   it { should have_many(:videos) }
 
   context 'when loading recent videos' do
-    it 'returns the 6 most recent vidoes in descending created at'
-    it 'returns all videos if less than 6 in descending created at'
+    before do
+      @category = Category.create name: 'Comedy'
+    end
+    
+    it 'returns the 6 most recent vidoes in descending created at' do
+      v1 = Video.create title: 'monk', description: 'good movie', category: @category
+      v2 = Video.create title: 'monk', description: 'good movie', category: @category
+      v3 = Video.create title: 'monk', description: 'good movie', category: @category
+      v4 = Video.create title: 'monk', description: 'good movie', category: @category
+      v5 = Video.create title: 'monk', description: 'good movie', category: @category
+      v6 = Video.create title: 'monk', description: 'good movie', category: @category
+      v7 = Video.create title: 'monk', description: 'good movie', category: @category
+      v8 = Video.create title: 'monk', description: 'good movie', category: @category
+
+      results = @category.recent_videos
+      expect(results).to eq([v8, v7, v6, v5, v4, v3, v2])
+    end
+    it 'returns all videos if less than or equal 6 videos exist in descending created at' do
+      v1 = Video.create title: 'monk', description: 'good movie', category: @category
+      v2 = Video.create title: 'monk', description: 'good movie', category: @category
+      v3 = Video.create title: 'monk', description: 'good movie', category: @category
+
+      results = @category.recent_videos
+      expect(results).to eq([v3, v2, v1])
+    end
     context 'with the same created at' do
-      it 'sorts by created at and then sorts alphabetically by title'
+      it 'sorts by created at and then sorts alphabetically by title' do
+        v1 = Video.create title: 'mank', description: 'good movie', category: @category
+        v2 = Video.create title: 'mink', description: 'good movie', category: @category
+        v3 = Video.create title: 'monk', description: 'good movie', category: @category
+
+        results = @category.recent_videos
+        expect(results).to eq([v1, v2, v3])
+      end
     end
   end
 end
