7cb74ddd965a2f6d1adb7abbe5a43d1fca51ec22
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2018-04-02 11:48:54 +0800

    Adds pending category_specs for recent videos

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Category do
   it { should have_many(:videos) }
+
+  context 'when loading recent videos' do
+    it 'returns the 6 most recent vidoes in descending created at'
+    it 'returns all videos if less than 6 in descending created at'
+    context 'with the same created at' do
+      it 'sorts by created at and then sorts alphabetically by title'
+    end
+  end
 end
