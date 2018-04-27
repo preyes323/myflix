774c445478ab1bb4d665a50dbfaf1e49ce8bb806
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2018-04-27 15:13:45 +0800

    Updates controller action for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = 'Your registration is successful'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :full_name)
+  end
 end
