6f9328fd6e88ed8d63f54a50ebf3a08c0f7d3b13
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2018-05-03 07:46:27 +0800

    Implements create action; WIP: user authentication method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    user = User.find_by email: params[:email]
+
+    if user && user.authenticate(params[:password])
+      flash[:success] = "Welcome, you've logged in."
+      session[:user] = user.id
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'There is something wrong with your username and password.'
+      render 'new'
+    end
+  end
 end
