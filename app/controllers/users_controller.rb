03ef13096337ee6e9e0922bdcadb51871002ee0e
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2018-05-02 08:58:47 +0800

    Fixes flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      flash[:notice] = 'Your registration is successful'
+      flash[:success] = 'Your registration is successful'
       redirect_to root_path
     else
       render 'new'
