e071c5b3fcef948f6d99f8ee4297cb3706d617c0
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2018-05-14 14:33:57 +0800

    Implements spec VideosController#search action

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -16,5 +16,18 @@ RSpec.describe VideosController do
     end
   end
   
-  describe 'GET search'
+  describe 'GET search' do
+    it 'sets @videos for authenticated uses based on :search_term' do
+      session[:user] = Fabricate(:user).id      
+      video = Fabricate(:video)
+      get :search, params: { search_term: video.title }
+      expect(assigns(:videos)).to eq([video])
+    end
+
+    it 'redirects to sign-in page for unauthenticated users' do
+      video = Fabricate(:video)
+      get :search, params: { search_term: video.title }      
+      expect(response).to redirect_to login_path      
+    end
+  end
 end
