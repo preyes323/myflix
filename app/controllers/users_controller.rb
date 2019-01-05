3a981a011915a56ba326b1c59a0dcaaf5d28a91e
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2019-01-05 16:00:28 +0800

    fix: add check that invitation token is not present

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       handle_invitation
       
@@ -45,7 +44,7 @@ class UsersController < ApplicationController
   end
 
   def handle_invitation
-    if params[:invitation_token]
+    if params[:invitation_token].empty?
       invitation = Invitation.find_by_token params[:invitation_token]
       @user.follow(invitation.inviter)
       invitation.inviter.follow(@user)
